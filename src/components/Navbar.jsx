859b8e18c298487f4501974154687cea656c5e08
ErlanPrambudi <130718497+ErlanPrambudi@users.noreply.github.com>
2025-01-10 11:19:37 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
+"use client";
+
+import { Suspense } from "react";
 import NavbarItem from "./NavbarItem";
 
 const Navbar = () => {
   return (
-    <div className="flex dark:bg-gray-600 bg-amber-100 lg:text-lg p-4 justify-center gap-4">
-      <NavbarItem title="Trending" param="fetchTrending" />
-      <NavbarItem title="Top Rated" param="fetchTopRated" />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex dark:bg-gray-600 bg-amber-100 lg:text-lg p-4 justify-center gap-4">
+        <NavbarItem title="Trending" param="fetchTrending" />
+        <NavbarItem title="Top Rated" param="fetchTopRated" />
+      </div>
+    </Suspense>
   );
 };
 export default Navbar;
